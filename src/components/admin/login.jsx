d54f87969f5529674b112b0699c1ce975105da3c
DESKTOP-D2GC4C5\yulis <jesus.santos@ujed.mx>
2024-11-27 11:06:12 -0600

    cambiar ruta de local a producción

--- src/components/admin/login.jsx
+++ src/components/admin/login.jsx
@@ -30,7 +30,7 @@ export default function Login() {
 
         try {
             const response = await axios.post(
-                'http://127.0.0.1:8000/users/login/', 
+                'https://movilidadback.ujed.mx/users/login/', 
                 loginData, 
                 {
                     headers: {
